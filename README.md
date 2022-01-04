24159b9656617979a74eb304d573fcfaebe3bb99
lukasjoc <jochamlu@gmail.com>
2022-01-04 20:06:16 +0100

    decide to use set implementation from glib

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# pysets
+# sets
 
-> "pythonic" sets and set functions in C
-
-INFO: This Implementation uses void pointers (void*) to work with any type
-
-Reference Class: https://docs.python.org/3/library/stdtypes.html#set
+> sets and set functions
 
---
+++ main.c
@@ -0,0 +1,34 @@
+#define _XOPEN_SOURCE 600
+
+#include <stdio.h>
+#include <glib.h>
+#include <gmodule.h>
+
+int main() {
+
+    GHashTable *table = g_hash_table_new(NULL, NULL);
+    g_hash_table_insert(table, (gpointer)"a", (gpointer)100);
+    g_hash_table_insert(table, (gpointer)"b", (gpointer)"HALLO");
+    g_hash_table_insert(table, (gpointer)"c", (gpointer)300);
+
+    // GList *keys = g_hash_table_get_keys(table);
+    // printf("%s\n", (char *)keys);
+    //
+    //
+    gpointer key, value;
+    GHashTableIter iter;
+
+    g_hash_table_iter_init(&iter, table);
+
+    while(g_hash_table_iter_next (&iter, &key, &value)) {
+        if(value != NULL) {
+            printf("%s : %d\n", (char *)(key), GPOINTER_TO_INT(value));
+        }
+    }
+
+
+    //g_hash_table_destroy(table);
+
+    return EXIT_SUCCESS;
+}
+
